7040bfcd78c76bbe37daefff3b6846eb98371021
Joaocosmo <dao.joaofilho@gmail.com>
2022-04-07 22:05:58 -0300

    integracao do espresso com recyclerview do app

--- app/src/androidTest/java/com/xuaum/cstv/AppUnitTest.kt
+++ app/src/androidTest/java/com/xuaum/cstv/AppUnitTest.kt
@@ -1,40 +1,66 @@
 package com.xuaum.cstv
 
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.*
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
-import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.xuaum.cstv.ui.home.MatchesAdapter
+import org.hamcrest.Matcher
 import org.hamcrest.Matchers.allOf
-import org.hamcrest.Matchers.not
 import org.junit.Assert.assertTrue
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+
 
 @RunWith(AndroidJUnit4::class)
 class AppUnitTest {
     @get:Rule
     var activityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
 
+    fun waitForItems(): ViewAction {
+        return object : ViewAction {
+            override fun getConstraints(): Matcher<View>? {
+                return allOf(
+                    isAssignableFrom(RecyclerView::class.java),
+                    isDisplayed()
+                )
+            }
+
+            override fun getDescription(): String {
+                return "Wait for items of View to appear."
+            }
+
+            override fun perform(uiController: UiController, view: View?) {
+                val recyclerView: RecyclerView = view as RecyclerView
+
+                while (recyclerView.adapter?.itemCount == 0) {
+                        uiController.loopMainThreadForAtLeast(200)
+                    }
+
+            }
+        }
+    }
+
+//    @Before
+//    fun waitUntilLoaded() {
+//        onView(withId(R.id.matches_container)).perform(waitForItems())
+//    }
+
     @Test
     fun clickCards_checkLoadingDetails() {
-        onView(
-            allOf(
-                withId(R.id.matches_loading),
-                isDisplayed()
-            )
-        ).check(matches(not(doesNotExist())))
 
-        onView(withId(R.id.matches_container)).check(is)
+        onView(withId(R.id.matches_container)).perform(waitForItems())
+        onView(withId(R.id.matches_container))
             .perform(
-                RecyclerViewActions.actionOnItem<MatchesAdapter.MatchViewHolder>(
-                    isDisplayed(),
+                RecyclerViewActions.actionOnItemAtPosition<MatchesAdapter.MatchViewHolder>(
+                    0,
                     click()
                 )
             )
